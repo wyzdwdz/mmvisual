ba53572179aa248a1ed5702e34fda54b02da47b4
wyzdwdz <wyzdwdz@gmail.com>
2025-07-07 13:02:20 +0200

    LICENSE and README

---
+++ README.md
@@ -0,0 +1,1 @@
+# Marvelmind superbeacon visualizer
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,9 @@
+// Copyright 2025 wyzdwdz <wyzdwdz@gmail.com>
+//
+// Licensed under the MIT license <LICENSE or https://opensource.org/licenses/MIT>.
+// This file may not be copied, modified, or distributed except according to
+// those terms.
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
